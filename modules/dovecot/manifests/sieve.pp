cc3f54e745778621850d5f9481434aae49fa269c
David Arroyo <droyo@aqwari.net>
2014-03-29 19:41:36 -0400

    Allow files or templates for sieve scripts

--- modules/dovecot/manifests/sieve.pp
+++ modules/dovecot/manifests/sieve.pp
@@ -1,4 +1,4 @@
-define dovecot::sieve($source) {
+define dovecot::sieve($source = undef, $content = undef) {
   realize File['/var/lib/dovecot/sieve']
 
   file{"/var/lib/dovecot/sieve/${name}.sieve":
@@ -6,5 +6,6 @@ define dovecot::sieve($source) {
     group => 'root',
     mode => '0644',
     source => $source,
+    content => $content,
   } ~> Exec['compile sieve scripts']
 }
