437df18e7448befe4851eca34894f1aaf88049c7
David Arroyo <darroyo@constantcontact.com>
2015-05-18 07:48:05 -0700

    add missing param

--- manifests/site.pp
+++ manifests/site.pp
@@ -95,6 +95,7 @@ nginx::ssl_server{'blog':
 nginx::ssl_server{'camlistore':
   server_name => 'camlistore.aqwari.net',
   listen => 443,
+  root => '/var/empty',
   ssl_certificate => '/etc/pki/tls/certs/nginx.pem',
   ssl_certificate_key => '/etc/pki/tls/private/nginx.pem',
   ssl_trusted_certificate => '/etc/pki/tls/certs/startss-chain.pem',
