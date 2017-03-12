2ff516ffa55bab2c4772ffb9e2fd4ee67e3b61f7
David Arroyo <droyo@aqwari.net>
2017-03-11 21:17:13 -0500

    file instead of augeas

--- modules/gcsfuse/manifests/init.pp
+++ modules/gcsfuse/manifests/init.pp
@@ -2,14 +2,11 @@ class gcsfuse {
   if $::osfamily == 'Debian' {
     $uri = 'http://packages.cloud.google.com/apt'
     $dist = "gcsfuse-${::lsbdistcodename}"
-    augeas{'add gcsfuse repo':
-      context => "/files/etc/apt/sources.list.d/google-cloud.list/",
-      changes => [
-          "set *[distribution = '${dist}']/type deb",
-          "set *[distribution = '${dist}']/uri '${uri}'",
-          "set *[distribution = '${dist}']/distribution gcsfuse-${::lsbdistcodename}",
-          "set *[distribution = '${dist}']/component main",
-      ],
+    file{'/etc/apt/sources.list.d/gcsfuse.list',
+      owner => 'root',
+      group => 'root',
+      mode => '0644',
+      content => "deb ${uri} ${dist} main\n",
     } ->
     package{'gcsfuse':
       ensure => 'installed',
