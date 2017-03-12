284e890dbeb4bb69ce865e1ede319fd16214b802
David Arroyo <droyo@aqwari.net>
2017-03-11 20:53:57 -0500

    use official go binaries

--- modules/golang/manifests/init.pp
+++ modules/golang/manifests/init.pp
@@ -1,16 +1,28 @@
-class golang(
-  $path = '/usr/local/go',
-) {
-  if $::osfamily == 'RedHat' {
-    include yumrepos::epel
-    Class['yumrepos::epel'] -> Package['golang']
+# Installs official Go distribution at /usr/local/go
+class golang {
+  $version = '1.8'
+  $arch = 'amd64'
+  $tarball = "go${version}.linux-${arch}.tar.gz"
+  $uri = "https://storage.googleapis.com/golang/${tarball}"
+
+  $path = '/usr/local/go'
+
+  package{'golang':
+    ensure => absent,
   }
-  
   file{$path:
     ensure => directory,
-  }
-  
-  package{'golang':
-    ensure => installed,
+  } ->
+  exec{"/usr/bin/curl -O '${uri}'":
+    creates => "${path}/${tarball}",
+    cwd     => $path,
+  } ->
+  exec{"/bin/tar --strip=1 -xzf '${tarball}'"
+    cwd    => $path,
+    onlyif => "/bin/test '${tarball}' -nt VERSION",
+  } ->
+  file{'/usr/bin/go':
+    ensure => link,
+    target => "${path}/bin",
   }
 }
