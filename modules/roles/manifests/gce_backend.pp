4967255840beb861b1d1133aa851085f7c16ddae
David Arroyo <droyo@aqwari.net>
2017-03-12 17:14:40 -0400

    Stop puppet on gce

--- modules/roles/manifests/gce_backend.pp
+++ modules/roles/manifests/gce_backend.pp
@@ -4,4 +4,8 @@ class roles::gce_backend {
   include profiles::gogive
   include profiles::webserver
   include profiles::blog_gcs
+
+  # Using preemptible instances, don't last longer than 24 hrs. No point running
+  # the puppet agent.
+  service{'puppet': ensure => stopped, enabled => false }
 }
