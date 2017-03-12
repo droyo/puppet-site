2d89538f19d43f75a239d348de610b7ed7dc8dc9
David Arroyo <droyo@aqwari.net>
2017-03-12 19:38:41 -0400

    rm gcsfuse

--- modules/profiles/manifests/blog_gcs.pp
+++ modules/profiles/manifests/blog_gcs.pp
@@ -1,6 +1,5 @@
 # Generates hugo blog and uploads it to a GCS bucket
 class profiles::blog_gcs {
-  include ::gcsfuse
   include ::hugo
   vcsrepo{'/srv/hugo':
     ensure   => 'present',
@@ -9,16 +8,11 @@ class profiles::blog_gcs {
     provider => 'git',
     source   => 'git://github.com/droyo/blog.git',
     require  => Class['::hugo::user'],
-  } ~>
-  exec{'apt-get update':
-    refreshonly => true,
-  } ->
-  gcsfuse::mount{'/srv/hugo/public':
-    user   => 'hugo',
-    bucket => 'blog.aqwari.net',
   }
-
+  cron{'upload blog to GCS bucket':
+    minute  => fqdn_rand(60),
+    command => 'gsutil -m cp -r public/* gs://blog.aqwari.net',
+    user    => 'hugo',
+  }
   Vcsrepo['/srv/hugo'] -> Class['::hugo']
-  Vcsrepo['/srv/hugo'] -> Gcsfuse::Mount['/srv/hugo/public']
-  Gcsfuse::Mount['/srv/hugo/public'] -> Class['::hugo::service']
 }
